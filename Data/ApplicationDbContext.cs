1feffc2c75360255c36147ac0e7ffab51ac61aec
Student <student@abv.bg>
2024-12-05 09:56:44 +0200

    sdfgs

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace za_ocenka.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.Migrate();
+        }
+        public DbSet<Id> Id { get; set; }
+        public DbSet<Name> Name { get; set; }
+        public DbSet<Price> Price { get; set; }
+        public DbSet<Quantity> Quantity { get; set; }
+    }
+}
---
+++ Data/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace za_ocenka.Data.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Quantity { get; set; }
+    }
+}
